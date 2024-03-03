e42da5d0f88ecaf6b4f6815b3d938c055e5e9ba7
EmilioC1 <ec2736@princeton.edu>
2024-03-03 17:06:51 -0500

    trying to deal with concurrency

--- client/main.go
+++ client/main.go
@@ -46,7 +46,7 @@ func main() {
 			// fmt.Println(string(body))
 			resChan <- string(body)
 			// fmt.Printf("received output from server from request %d\n", index)
-		}(2)
+		}(i)
 	}
 	for i := 0; i < resChanLen; i++ {
 		s := <-resChan
