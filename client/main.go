468b7466dc1dc90163efa9a75d8b2613a8fd29ea
EmilioC1 <ec2736@princeton.edu>
2024-03-02 20:21:53 -0500

    changing something wit the networking

--- client/main.go
+++ client/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	params := url.Values{}
 	params.Add("id", fmt.Sprint(1))
-	url := "http://Server-VM:80/"
+	url := "http//40.71.219.66:80/"
 	// + "?" + params.Encode()
 	fmt.Println("About to make request")
 	res, err := http.Get(url)
