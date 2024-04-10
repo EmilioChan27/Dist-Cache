d481d62cfc471f306d200acdbe6340d8b2ba334f
EmilioC1 <ec2736@princeton.edu>
2024-04-10 13:20:06 -0400

    trying something else

--- client/client.go
+++ client/client.go
@@ -147,7 +147,7 @@ func actualTest(numClients int, testDuration time.Duration) {
 	src := rand.NewSource(int64(maxId))
 	zipf := rand.NewZipf(rand.New(src), 1.5, 8, uint64(maxId))
 	actualNumClients := 0
-	file, err := os.Create(fmt.Sprintf("%dclients-%vduration-pause5+30s-nocache.txt", numClients, testDuration))
+	file, err := os.Create(fmt.Sprintf("%dclients-%vduration-pause5+30s-cache.txt", numClients, testDuration))
 	c.CheckErr(err)
 	file.WriteString("overallTimer := time.NewTimer(testDuration)\nmaxId := 51476\nsrc := rand.NewSource(int64(maxId))\nzipf := rand.NewZipf(rand.New(src), 1.5, 8, uint64(maxId))\n")
 	file.WriteString("Hello????")
