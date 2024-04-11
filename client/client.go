8f374c681de99925556688526fae61f3517a3644
EmilioC1 <ec2736@princeton.edu>
2024-04-10 23:00:46 -0400

    changed some stuff

--- client/client.go
+++ client/client.go
@@ -143,7 +143,7 @@ func actualTest(numClients int, testDuration time.Duration) {
 	clients := make(chan int, numClients)
 	writes := make(chan int, 100)
 	overallTimer := time.NewTimer(testDuration)
-	maxId := 52261
+	maxId := 52262
 	src := rand.NewSource(int64(maxId))
 	zipf := rand.NewZipf(rand.New(src), 1.5, 8, uint64(maxId))
 	actualNumClients := 0
