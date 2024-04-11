2ef24e1e7dd427382566322b3f9f7542defece3d
EmilioC1 <ec2736@princeton.edu>
2024-04-11 19:25:52 -0400

    adding another test

--- client/client.go
+++ client/client.go
@@ -15,13 +15,13 @@ import (
 )
 
 func main() {
-	actualTest(300, 8*time.Minute)
+	actualTest(500, 8*time.Minute)
 }
 func actualTest(numClients int, testDuration time.Duration) {
 	clients := make(chan int, numClients)
 	writes := make(chan int, 1000)
 	overallTimer := time.NewTimer(testDuration)
-	maxId := 53073
+	maxId := 53096
 	src := rand.NewSource(int64(maxId))
 	zipf := rand.NewZipf(rand.New(src), 1.5, 8, uint64(maxId))
 	actualNumClients := 0
