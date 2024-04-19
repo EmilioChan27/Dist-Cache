6a9d9c4b454d86f7ed482f6fd2763773c714d4aa
EmilioC1 <ec2736@princeton.edu>
2024-04-18 21:53:09 -0400

    changed the startup distance so that we wouldn't get teh worker error

--- client/client.go
+++ client/client.go
@@ -82,7 +82,7 @@ func actualTest(numClients int, testDuration time.Duration, waitTimeMean int, ru
 			}
 		default:
 			if actualNumClients < numClients {
-				time.Sleep(5 * time.Millisecond)
+				time.Sleep(200 * time.Millisecond)
 				clients <- 1
 				actualNumClients++
 				fmt.Printf("Current numClients: %d\n", actualNumClients)
